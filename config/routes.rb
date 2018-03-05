5a2eb42ba427b17c65a26e769aa3c76324138fe4
arturo <informa.fj@gmail.com>
2018-03-05 11:09:16 -0300

    Primer commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
-  resources :products, only: [:index] do
-    resources :orders, only: :create
-  end
-  
-  resources :orders, only: [:index, :destroy] do
-    collection do
-      delete 'empty_cart'
-    end
-  end
-  
-  
-  
-  root 'products#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	devise_for :users
+	
+	resources :products, only: [:index] do
+		resources :orders, only: :create
+	end
+	
+	resources :orders, only: [:index, :destroy] do
+		collection do
+			delete 'empty_cart'
+		end
+	end
+	
+	
+	
+	root 'products#index'
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
